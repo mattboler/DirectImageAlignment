29ec36be0f197e0b6f970d3f23459fe3e7b761eb
Matt Boler <meb0054@auburn.edu>
2020-04-03 10:26:23 -0500

    Works on 2 images

--- Code/doAlignment.m
+++ Code/doAlignment.m
@@ -11,17 +11,18 @@ d1 = double(imread('depth/1305031102.262886.png'))/5000;
 % approximately -0.0021    0.0057    0.0374   -0.0292   -0.0183   -0.0009
 
 %%
+%{
 K = [ 535.4  0 320.1;	0 539.2 247.6; 0 0 1];
-c1 = double(imreadbw('rgb/1341847980.722988.png'));
-c2 = double(imreadbw('rgb/1341847982.998783.png'));
+c2 = double(imreadbw('rgb/1341847980.722988.png'));
+c1 = double(imreadbw('rgb/1341847982.998783.png'));
 
 
-d1 = double(imread('depth/1341847980.723020.png'))/5000;
-d2 = double(imread('depth/1341847982.998830.png'))/5000;
+d2 = double(imread('depth/1341847980.723020.png'))/5000;
+d1 = double(imread('depth/1341847982.998830.png'))/5000;
 
 % result:
 %  approximately -0.2894 0.0097 -0.0439  0.0039 0.0959 0.0423
-
+%}
 
 %%
 % TODO
@@ -31,9 +32,14 @@ d2 = double(imread('depth/1341847982.998830.png'))/5000;
 
 % Initilize estimate
 xi = [0 0 0 0 0 0]';
+prevErr = 1e10;
+threshold = 1e-5;
+
+figure(1)
+calcErr(c1, d1, c2, xi, K, 1);
 
 % Using 5 pyramid levels
-for level = 5 : -1 : 1
+for level = 6 : -1 : 1
     % 1. Get downscaled image, depth image, and K
     KLevel = downscaleK(K, level);
     imageRef = downscaleImage(c1, level);
@@ -42,12 +48,25 @@ for level = 5 : -1 : 1
     
     
     % 2. For at most 20 iterations, perform Guass-Newton to minimize error
-    prevErr = 1e10;
-    for i = 1 : 20
+    for i = 1 : 15
+        [jacobian, r] = deriveErrorNumeric(imageRef, depthRef, image, xi, KLevel);
+        validIdx = ~isnan(sum(jacobian, 2));
+        validJacobian = jacobian(validIdx, :);
+        validR = r(validIdx, :);
         
+        gaussNewtonUpdate = -inv(validJacobian' * validJacobian) * validJacobian' * validR;
+        xi = se3Log( se3Exp(gaussNewtonUpdate) * se3Exp(xi) );
+        
+        err = mean(validR .* validR)
+        if abs(prevErr - err) < threshold
+            break
+        else
+            prevErr = err;
+        end
     end
-    
-    % 3. Set pose estimate and move on to next level
-    
     level
 end
+
+xi
+figure(2)
+calcErr(imageRef, depthRef, image, xi, KLevel, 1);
